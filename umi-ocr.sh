1ad9e7e067336fc1735f413e36de819edeb32539
hiroi-sora <56373419+hiroi-sora@users.noreply.github.com>
2024-07-27 23:16:39 +0800

    更新启动脚本，优化输出，增加 HEADLESS 无头模式

--- umi-ocr.sh
+++ umi-ocr.sh
@@ -7,20 +7,39 @@ current_dir=$(pwd)
 # 获取脚本的绝对路径并写入环境变量
 export UMI_APP_PATH=$(realpath ${BASH_SOURCE[0]})
 
-# 切换环境
+# 检查 Python 环境
+#   嵌入式环境
 if [ -f "UmiOCR-data/.embeddable/activate.sh" ]; then
     cd UmiOCR-data/.embeddable
     source activate.sh
     cd $current_dir
-    echo "激活嵌入式 Python 环境。"
+    echo "Use the Python embeddable environment."
+#   虚拟环境
 elif [ -f "UmiOCR-data/.venv/bin/activate" ]; then
     source UmiOCR-data/.venv/bin/activate
-    echo "激活 Python 虚拟环境。"
+    echo "Use the Python virtual environment."
+#   默认环境
 else
-    echo "使用默认 Python 环境。"
+    echo "Use the default Python environment."
 fi
 
-echo "工作目录: $(pwd)"
+echo "pwd: $(pwd)"
+
+# 检查环境变量 HEADLESS ，为 true 时启用无头模式
+if [ "$HEADLESS" == "true" ]; then
+  echo "Use headless mode."
+  Xvfb :99 -screen 0 1024x768x16 & export DISPLAY=:99
+else
+  echo "Use GUI mode."
+  if [ -z "$DISPLAY" ]; then
+    echo "Error: \$DISPLAY is not set."
+    exit 1
+  fi
+  # 使用 docker GUI 模式时，
+  # 应挂载主机的 /tmp/.X11-unix 目录到容器内的相同路径。
+  # 该操作可在 docker run 命令中完成，如下所示：
+  # docker run -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY ……
+fi
 
 # 通过指定环境中的Python解释器，启动主程序，传入命令行指令
 python3 UmiOCR-data/main_linux.py "$@"
